5bf3c612d427ffc63aff805b82d62cd48d453d80
Christian R Garcia <christian.g21@gmail.com>
2017-02-15 02:12:26 -0600

    Almost finished with homework 2

--- Homework/Homework 2/LUfactor.m
+++ Homework/Homework 2/LUfactor.m
@@ -1,8 +1,14 @@
-function [ output_args ] = LUfactor(matrix, size)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-
-
+function [L,U] = LUfactor(A,n)
+%LUfactor Factors A and outputs (L)ower and (U)pper triangular matrices
+%   This function takes a n by n matrix (A) and LU factorizes it in
+%   order to get a an upper triangular matrix (U). It then gets the lower
+%   triangular matrix (L) from the equation L = A/U. Both are L and U are then
+%   outputted.
+U = A;
+for i = 2:n
+    for j = 1:(i-1)
+            U(i,j:n) = U(i,j:n) - ((U((i-(i-j)),(j:n)))*(U(i,j)/U((i-(i-j)),j)));
+    end
 end
-
+L = A/U;
+end
