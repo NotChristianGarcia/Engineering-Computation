eb46afb0c29c0b0952cbd44ca7ad1a4938baaebd
Christian R Garcia <christian.g21@gmail.com>
2017-02-15 03:25:20 -0600

    Finished homework 2

--- Homework/Homework 2/HW2_4b.m
+++ Homework/Homework 2/HW2_4b.m
@@ -1,6 +1,9 @@
-x = .5;
-s = [10^(-12),10^(-10),10^(-8),10^(-6),10^(-4),10^(-2)];
-for k = 1:length(s)
-     h = s(k);
-     stepsize(k) = ((((1/(sqrt(2*pi)))*exp(((-(x+h)).^2)/2)) - ((1/(sqrt(2*pi)))*exp(((-(x)).^2)/2)))/h);
+x = .5; % Setting x = .5
+s = [10^(-12),10^(-10),10^(-8),10^(-6),10^(-4),10^(-2)]; % Creating the different stepsizes in an array
+for k = 1:length(s) % Moving k through all stepsizes
+     h = s(k); % Initializing the working stepsize for each stepsize in matrix
+     stepsize(k) = ((((1/(sqrt(2*pi)))*exp(((-(x+h)).^2)/2)) - ((1/(sqrt(2*pi)))*exp(((-(x)).^2)/2)))/h); % Solving for each stepsize and throwing data in a matrix
+end
+for j = 1:length(stepsize) % Going through every result from the previous loop
+     fprintf('For stepsize %d the result was %d.\n',s(j),stepsize(j)) % Displaying stepsize with the result it had
 end
--- Homework/Homework 2/LUfactor.m
+++ Homework/Homework 2/LUfactor.m
@@ -4,11 +4,11 @@ function [L,U] = LUfactor(A,n)
 %   order to get a an upper triangular matrix (U). It then gets the lower
 %   triangular matrix (L) from the equation L = A/U. Both are L and U are then
 %   outputted.
-U = A;
-for i = 2:n
-    for j = 1:(i-1)
-            U(i,j:n) = U(i,j:n) - ((U((i-(i-j)),(j:n)))*(U(i,j)/U((i-(i-j)),j)));
+U = A; % Sets Upper = A, just initially
+for i = 2:n % Goes through row 2 till end
+    for j = 1:(i-1) % Goes through col 1 through (i-1), which is the cols which should have 0's
+            U(i,j:n) = U(i,j:n) - ((U((i-(i-j)),(j:n)))*(U(i,j)/U((i-(i-j)),j))); % Solves for 0's where they should be; isn't it a lovely equation?
     end
 end
-L = A/U;
+L = A/U; % Solves for Lower Triangular from A and the Upper Triangular that we solved for
 end
